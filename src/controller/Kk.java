aeb7b6414835bf6073e61e5ba4cc1ecc6b303eed
vmonmai01 <victormontenegromaillo@gmail.com>
2023-03-14 09:56:58 +0100

    feat (add structure to proyect)

---
+++ src/controller/Kk.java
@@ -0,0 +1,19 @@
+package controller;
+
+public class Kk {
+	//SimpleDateFormat sdfSQL = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+//    java.util.Date fechaNac = null;
+//
+//    try {
+//        fechaNac = sdfSQL.parse("19/03/1977 15:33:00");
+//    } catch (ParseException e) {
+//        // TODO Auto-generated catch block
+//        e.printStackTrace();
+//    }
+//
+//    ps.setDate(0, java.sql.Date(fechaNac.getTime()));
+//
+//    int filasAfectadas = ps.executeUpdate;
+
+}
